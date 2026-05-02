8506ba8f3987ca0d9d0e1d1dff57ef700b2a4524
musishere <mustafabukhari333@gmail.com>
2026-05-02 14:45:07 +0500

    Implement text summarization functionality using Hugging Face model in text_loader.py

--- LangChain_Document_Loaders/text_loader.py
+++ LangChain_Document_Loaders/text_loader.py
@@ -1,6 +1,23 @@
 from langchain_community.document_loaders import TextLoader
+from langchain_huggingface import HuggingFaceEndpoint,ChatHuggingFace
+from dotenv import load_dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
 
+
+load_dotenv()
+llm = HuggingFaceEndpoint(repo_id="deepseek-ai/DeepSeek-R1")
+model = ChatHuggingFace(llm=llm)
+parser = StrOutputParser()
+
+
+prompt = PromptTemplate(
+    template = 'Generate a summary for text {text}',
+    input_variables=['text']
+)
 loader = TextLoader("/Users/macbookpro/Documents/Genrative AI/LangChain_Document_Loaders/random_text.txt",encoding='utf-8')
 
 docs = loader.load()
-print(docs[0])
+chain = prompt | model | parser
+response = chain.invoke({'text':docs[0].page_content})
+print(response)
